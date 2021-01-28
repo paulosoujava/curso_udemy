4e1aa2d57bc28b3052a7293ed82dc30d51571fe4
Paulo Oliveira <paulosoujava@gmail.com>
2021-01-28 11:49:25 -0300

    finish code

--- plataform_channels_demo/android/app/src/main/java/br/com/paulojorge/plataform_channels_demo/MainActivity.java
+++ plataform_channels_demo/android/app/src/main/java/br/com/paulojorge/plataform_channels_demo/MainActivity.java
@@ -11,28 +11,34 @@ import io.flutter.plugin.common.MethodChannel;
 
 
 public class MainActivity extends FlutterActivity {
-
+    private Geolocation geolocation;
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
         BinaryMessenger binaryMessenger =flutterEngine.getDartExecutor().getBinaryMessenger();
-        MethodChannel methodChannel = new MethodChannel(binaryMessenger,"br.com.paulojorge.plataform_channels_demo");
-        methodChannel.setMethodCallHandler((call, result) -> {
-            if(call.method.equals("porraaaaa")){
-
-                String version = getVersion();
-                result.success(version );
-
-            }else{
-                result.notImplemented();
-            }
-
-        });
+       // MethodChannel methodChannel = new MethodChannel(binaryMessenger,"br.com.paulojorge.plataform_channels_demo");
+        this.geolocation = new Geolocation(this, flutterEngine);
+//        methodChannel.setMethodCallHandler((call, result) -> {
+//            if(call.method.equals("porraaaaa")){
+//
+//                String version = getVersion();
+//                result.success(version );
+//
+//            }else{
+//                result.notImplemented();
+//            }
+//
+//        });
     }
 
     String getVersion(){
         int sdkVersion = Build.VERSION.SDK_INT;
         String release= Build.VERSION.RELEASE;
         return "Android version: "+ sdkVersion +" release: "+release;
+    }
+    @Override
+    protected void onDestroy() {
+        this.geolocation.stop();
+        super.onDestroy();
     }
 }
